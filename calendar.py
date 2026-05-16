4e779a5e04995f093063ff8042770c7d0e869fbb
Jazushii <jazushii7@gmail.com>
2026-05-16 16:28:16 +1200

    Task 2.1 & 3.1
    copied the main frame code to orderlist.py and calendar.py, and changed the navbar to correspond to the current interface

---
+++ calendar.py
@@ -0,0 +1,37 @@
+import tkinter as tk
+
+# Task 1.1 & 1.2
+calendar = tk.Tk()
+calendar.title('Cake Desire')
+calendar.geometry('960x540+195+75')
+calendar.resizable(False, False)
+
+# Task 1.2
+frm_title = tk.Frame(calendar, height=60, bg='#FFD333', bd=1, relief='solid')
+frm_title.pack(fill=tk.X)
+frm_title.pack_propagate(False)
+
+navbar_inventory = tk.Frame(calendar, width=110, height=160, bg='#FEF67F')
+navbar_inventory.place(x=0, y=60)
+navbar_inventory.pack_propagate(False)
+navbar_orderlist = tk.Frame(calendar, width=110, height=160, bg='#FEF67F')
+navbar_orderlist.place(x=0, y=220)
+navbar_orderlist.pack_propagate(False)
+navbar_calendar = tk.Frame(calendar, width=110, height=160, bg='#FFC957')
+navbar_calendar.place(x=0, y=380)
+navbar_calendar.pack_propagate(False)
+
+# Task 1.3 & 1.4
+cd_logo = tk.PhotoImage(file='cake-desire-logo.png')
+lbl_title = tk.Label(frm_title, text='Cake Desire ', font=('Ink Free', 24, 'bold'), image=cd_logo, compound='right', bg='#FFD333')
+lbl_title.pack(fill="both", expand=True)
+
+btn_inventory = tk.Button(navbar_inventory, text='Inventory', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957')
+btn_inventory.pack(fill="both", expand=True)
+btn_orderlist = tk.Button(navbar_orderlist, text='Order\nList', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957')
+btn_orderlist.pack(fill="both", expand=True)
+btn_calendar = tk.Button(navbar_calendar, text='Calendar', font=('Segoe Print', 14, 'bold'), bg='#FFC957', activebackground='#FFC957')
+btn_calendar.pack(fill="both", expand=True)
+
+# Task 1.1
+calendar.mainloop()
--- inventory.py
+++ inventory.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 
-# Task 1.1
+# Task 1.1 & 1.2
 inventory = tk.Tk()
-inventory.title('Inventory - Cake Desire')
+inventory.title('Cake Desire')
 inventory.geometry('960x540+195+75')
 inventory.resizable(False, False)
 
@@ -11,35 +11,27 @@ frm_title = tk.Frame(inventory, height=60, bg='#FFD333', bd=1, relief='solid')
 frm_title.pack(fill=tk.X)
 frm_title.pack_propagate(False)
 
-frm_navbar = tk.Frame(inventory, width=110, bg='#FEF67F')
-frm_navbar.pack(fill=tk.Y, side='left')
-frm_navbar.pack_propagate(False)
-frm_active = tk.Frame(frm_navbar, width=110, height=160, bg='#FFC957')
-frm_active.pack(side='top')
-frm_active.pack_propagate(False)
+navbar_inventory = tk.Frame(inventory, width=110, height=160, bg='#FFC957')
+navbar_inventory.place(x=0, y=60)
+navbar_inventory.pack_propagate(False)
+navbar_orderlist = tk.Frame(inventory, width=110, height=160, bg='#FEF67F')
+navbar_orderlist.place(x=0, y=220)
+navbar_orderlist.pack_propagate(False)
+navbar_calendar = tk.Frame(inventory, width=110, height=160, bg='#FEF67F')
+navbar_calendar.place(x=0, y=380)
+navbar_calendar.pack_propagate(False)
 
-left_bd = tk.Frame(inventory, width=1, height=540, bg='black')
-left_bd.place(x=0, y=0)
-title_right_bd = tk.Frame(inventory, width=1, height=60, bg='black')
-title_right_bd.place(x=959, y=0)
-navbar_right_bd = tk.Frame(inventory, width=1, height=480, bg='black')
-navbar_right_bd.place(x=110, y=60)
-active_bottom_bd = tk.Frame(inventory, width=110, height=1, bg='black')
-active_bottom_bd.place(x=0, y=220)
-navbar_bottom_bd = tk.Frame(inventory, width=110, height=1, bg='black')
-navbar_bottom_bd.place(x=0, y=539)
-
-# Task 1.3
+# Task 1.3 & 1.4
 cd_logo = tk.PhotoImage(file='cake-desire-logo.png')
 lbl_title = tk.Label(frm_title, text='Cake Desire ', font=('Ink Free', 24, 'bold'), image=cd_logo, compound='right', bg='#FFD333')
-lbl_title.place(relx=0.5, rely=0.5, anchor='center')
+lbl_title.pack(fill="both", expand=True)
 
-lbl_active = tk.Label(frm_active, text='Inventory', font=('Segoe Print', 14, 'bold'), bg='#FFC957')
-lbl_active.place(relx=0.5, rely=0.5, anchor='center')
-lbl_navbar = tk.Label(frm_navbar, text='Order\nList', font=('Segoe Print', 14), bg='#FEF67F')
-lbl_navbar.place(relx=0.5, rely=0.5, anchor='center')
-lbl_navbar = tk.Label(frm_navbar, text='Calendar', font=('Segoe Print', 14), bg='#FEF67F')
-lbl_navbar.place(relx=0.5, rely=5/6, anchor='center')
+btn_inventory = tk.Button(navbar_inventory, text='Inventory', font=('Segoe Print', 14, 'bold'), bg='#FFC957', activebackground='#FFC957')
+btn_inventory.pack(fill="both", expand=True)
+btn_orderlist = tk.Button(navbar_orderlist, text='Order\nList', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957')
+btn_orderlist.pack(fill="both", expand=True)
+btn_calendar = tk.Button(navbar_calendar, text='Calendar', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957')
+btn_calendar.pack(fill="both", expand=True)
 
 # Task 1.1
 inventory.mainloop()
