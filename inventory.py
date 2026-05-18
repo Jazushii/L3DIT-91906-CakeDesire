db07390c86c422f8acc05787fc10370415f8a64e
Jazushii <jazushii7@gmail.com>
2026-05-18 14:20:28 +1200

    Task 1.1 & 1.2 - Improvement
    ive put more frames for easier coding (ie. main, navbar)
    ive also changed the variables for more flexible changes

--- inventory.py
+++ inventory.py
@@ -7,27 +7,35 @@ def OpenInventory():
     inventory.geometry('960x540+195+75')
     inventory.resizable(False, False)
 
+    main = tk.Frame(inventory, width=960, height=540)
+    main.pack()
+    main.pack_propagate(False)
+
     # Task 1.2
-    frm_title = tk.Frame(inventory, height=60, bg='#FFD333', bd=1, relief='solid')
-    frm_title.pack(fill=tk.X)
-    frm_title.pack_propagate(False)
+    title = tk.Frame(main, height=60, bg='#FFD333', bd=1, relief='solid')
+    title.pack(fill=tk.X)
+    title.pack_propagate(False)
 
-    navbar_inventory = tk.Frame(inventory, width=110, height=160, bg='#FFC957')
-    navbar_inventory.place(x=1, y=60)
-    navbar_inventory.pack_propagate(False)
-    navbar_orderlist = tk.Frame(inventory, width=110, height=160, bg='#FEF67F')
-    navbar_orderlist.place(x=1, y=220)
-    navbar_orderlist.pack_propagate(False)
-    navbar_calendar = tk.Frame(inventory, width=110, height=160, bg='#FEF67F')
-    navbar_calendar.place(x=1, y=380)
-    navbar_calendar.pack_propagate(False)
+    navbar = tk.Frame(main, width=110, height=480)
+    navbar.place(x=0, y=60)
+    navbar.pack_propagate(False)
 
-    navbar_bd_left = tk.Frame(inventory, width=1, height=480, bg='black')
+    navbar_inven = tk.Frame(navbar, width=110, height=160, bg='#FFC957')
+    navbar_inven.pack(fill=tk.X)
+    navbar_inven.pack_propagate(False)
+    navbar_order = tk.Frame(navbar, width=110, height=160, bg='#FEF67F')
+    navbar_order.pack(fill=tk.X)
+    navbar_order.pack_propagate(False)
+    navbar_calen = tk.Frame(navbar, width=110, height=160, bg='#FEF67F')
+    navbar_calen.pack(fill=tk.X)
+    navbar_calen.pack_propagate(False)
+
+    navbar_bd_left = tk.Frame(navbar, width=1, height=480, bg='black')
     navbar_bd_left.place(x=0, y=60)
 
     # Task 1.3 & 1.4
-    cd_logo = tk.PhotoImage(file='cake-desire-logo.png')
-    lbl_title = tk.Label(frm_title, text='Cake Desire ', font=('Ink Free', 24, 'bold'), image=cd_logo, compound='right', bg='#FFD333')
+    logo = tk.PhotoImage(file='cake-desire-logo.png')
+    lbl_title = tk.Label(title, text='Cake Desire ', font=('Ink Free', 24, 'bold'), image=logo, compound='right', bg='#FFD333')
     lbl_title.pack(fill="both", expand=True)
 
     def switch_orderlist():
@@ -40,12 +48,12 @@ def OpenInventory():
         inventory.destroy()
         OpenCalendar()
 
-    btn_inventory = tk.Button(navbar_inventory, text='Inventory', font=('Segoe Print', 14, 'bold'), bg='#FFC957', activebackground='#FFC957')
-    btn_inventory.pack(fill="both", expand=True)
-    btn_orderlist = tk.Button(navbar_orderlist, text='Order\nList', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=switch_orderlist)
-    btn_orderlist.pack(fill="both", expand=True)
-    btn_calendar = tk.Button(navbar_calendar, text='Calendar', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=switch_calendar)
-    btn_calendar.pack(fill="both", expand=True)
+    btn_inven = tk.Button(navbar_inven, text='Inventory', font=('Segoe Print', 14, 'bold'), bg='#FFC957', activebackground='#FFC957')
+    btn_inven.pack(fill="both", expand=True)
+    btn_order = tk.Button(navbar_order, text='Order\nList', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=switch_orderlist)
+    btn_order.pack(fill="both", expand=True)
+    btn_calen = tk.Button(navbar_calen, text='Calendar', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=switch_calendar)
+    btn_calen.pack(fill="both", expand=True)
 
     # Task 1.1
     inventory.mainloop()
