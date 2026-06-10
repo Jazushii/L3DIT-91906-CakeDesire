fe68ed871914ec0ed72e95c33a38f878634af972
Jazushii <jazushii7@gmail.com>
2026-06-11 09:37:28 +1200

    Code clean-up
    just cleaning up the layout of my coding, deciding on a main.py to house the main frame and navbar as its just the same throughout the code
    ive made the switching of interfaces work with the switch() function, but theres the problem of the 'active' navbar being the darker colour, yet now its just the orderlist thats dark, so ill fix that using variables and then clean the layout some more

---
+++ main.py
@@ -0,0 +1,69 @@
+import tkinter as tk
+from inventory import *
+from orderlist import *
+from calendar import *
+
+def interface(type):
+    # Task 1.1 & 1.2
+    root = tk.Tk()
+    root.title('Cake Desire')
+    root.geometry('960x540+195+75')
+    root.resizable(False, False)
+
+    main = tk.Frame(root, width=960, height=540)
+    main.pack()
+    main.pack_propagate(False)
+
+    # Task 1.2
+    title = tk.Frame(main, height=60, bg='#FFD333', bd=1, relief='solid')
+    title.pack(fill=tk.X)
+    title.pack_propagate(False)
+
+    navbar = tk.Frame(main, width=110, height=480)
+    navbar.place(x=0, y=60)
+    navbar.pack_propagate(False)
+
+    navbar_inven = tk.Frame(navbar, height=160, bg='#FEF67F')
+    navbar_inven.pack(fill=tk.X)
+    navbar_inven.pack_propagate(False)
+    navbar_order = tk.Frame(navbar, height=160, bg='#FFC957')
+    navbar_order.pack(fill=tk.X)
+    navbar_order.pack_propagate(False)
+    navbar_calen = tk.Frame(navbar, height=160, bg='#FEF67F')
+    navbar_calen.pack(fill=tk.X)
+    navbar_calen.pack_propagate(False)
+
+    navbar_bd_left = tk.Frame(navbar, width=1, height=480, bg='black')
+    navbar_bd_left.pack()
+
+    # Task 1.3 & 1.4
+    logo = tk.PhotoImage(file='cake-desire-logo.png')
+    lbl_title = tk.Label(title, text='Cake Desire ', font=('Ink Free', 24, 'bold'), image=logo, compound='right', bg='#FFD333')
+    lbl_title.pack(fill="both", expand=True)
+
+    def switch(switch):
+        root.destroy()
+        if switch == 'inventory':
+            interface('inventory')
+        if switch == 'orderlist':
+            interface('orderlist')
+        if switch == 'calendar':
+            interface('calendar')
+
+    btn_inven = tk.Button(navbar_inven, text='Inventory', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=lambda:switch('inventory'))
+    btn_inven.pack(fill="both", expand=True)
+    btn_order = tk.Button(navbar_order, text='Order\nList', font=('Segoe Print', 14, 'bold'), bg='#FFC957', activebackground='#FFC957', command=lambda:switch('orderlist'))
+    btn_order.pack(fill="both", expand=True)
+    btn_calen = tk.Button(navbar_calen, text='Calendar', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=lambda:switch('calendar'))
+    btn_calen.pack(fill="both", expand=True)
+
+    root.mainloop()
+
+    if type == 'inventory':
+        open_inventory(main)
+    if type == 'orderlist':
+        open_orderlist(main)
+    if type == 'calendar':
+        open_calendar(main)
+
+interface('inventory')
--- orderlist.py
+++ orderlist.py
@@ -1,61 +1,7 @@
 import tkinter as tk
 import json
 
-def OpenOrderList():
-    # Task 1.1 & 1.2
-    orderlist = tk.Tk()
-    orderlist.title('Cake Desire')
-    orderlist.geometry('960x540+195+75')
-    orderlist.resizable(False, False)
-
-    main = tk.Frame(orderlist, width=960, height=540)
-    main.pack()
-    main.pack_propagate(False)
-
-    # Task 1.2
-    title = tk.Frame(main, height=60, bg='#FFD333', bd=1, relief='solid')
-    title.pack(fill=tk.X)
-    title.pack_propagate(False)
-
-    navbar = tk.Frame(main, width=110, height=480)
-    navbar.place(x=0, y=60)
-    navbar.pack_propagate(False)
-
-    navbar_inven = tk.Frame(navbar, height=160, bg='#FEF67F')
-    navbar_inven.pack(fill=tk.X)
-    navbar_inven.pack_propagate(False)
-    navbar_order = tk.Frame(navbar, height=160, bg='#FFC957')
-    navbar_order.pack(fill=tk.X)
-    navbar_order.pack_propagate(False)
-    navbar_calen = tk.Frame(navbar, height=160, bg='#FEF67F')
-    navbar_calen.pack(fill=tk.X)
-    navbar_calen.pack_propagate(False)
-
-    navbar_bd_left = tk.Frame(navbar, width=1, height=480, bg='black')
-    navbar_bd_left.pack()
-
-    # Task 1.3 & 1.4
-    logo = tk.PhotoImage(file='cake-desire-logo.png')
-    lbl_title = tk.Label(title, text='Cake Desire ', font=('Ink Free', 24, 'bold'), image=logo, compound='right', bg='#FFD333')
-    lbl_title.pack(fill="both", expand=True)
-
-    def switch_inventory():
-        from inventory import OpenInventory
-        orderlist.destroy()
-        OpenInventory()
-    
-    def switch_calendar():
-        from calendar import OpenCalendar
-        orderlist.destroy()
-        OpenCalendar()
-
-    btn_inven = tk.Button(navbar_inven, text='Inventory', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=switch_inventory)
-    btn_inven.pack(fill="both", expand=True)
-    btn_order = tk.Button(navbar_order, text='Order\nList', font=('Segoe Print', 14, 'bold'), bg='#FFC957', activebackground='#FFC957')
-    btn_order.pack(fill="both", expand=True)
-    btn_calen = tk.Button(navbar_calen, text='Calendar', font=('Segoe Print', 14), bg='#FEF67F', activebackground='#FFC957', command=switch_calendar)
-    btn_calen.pack(fill="both", expand=True)
-
+def open_orderlist(main):
     # Task 2.2
     content = tk.Frame(main, width=850, height=480)
     content.place(x=110, y=60)
