04d03f761a747ea3dd1b89cbaa2621fc7c0dbbae
Jazushii <jazushii7@gmail.com>
2026-06-22 14:59:50 +1200

    Task 2.6.3 start and full layout change
    ive started 2.6.3 and the layout of my code is annoying me so once again ill be fixing the layout by taking out all the functions / make the things into a function

--- orderlist.py
+++ orderlist.py
@@ -1,143 +1,39 @@
 import tkinter as tk
 import json
+import os
 
 def open_orderlist(content):
-    # Task 2.2 Details & Entry Boxes
-    detail_lbls = [
-        'Customer Name:',
-        'Cake Flavour:',
-        'Due Date:',
-        '',
-        '',
-        'Cake Colour/s:',
-        'Cake Type:',
-        'Cake Shape:'
-    ]
+    create_details(content)
 
-    detail_ents = []
+    detail_save_lbls = ['customer_details',
+                       'cake_flavour',
+                       'due_day',
+                       'due_month',
+                       'due_year',
+                       'cake_colours',
+                       'cake_type',
+                       'cake_shape'
+                       ]
 
-    detail_num = 0
+    tier_save_lbls = ['layer', 'size_a', 'size_b']
 
-    for dr in range(3):
-        for dc in range(2):
-            frame = tk.Frame(content, width=265, height=62, bd=1.5, relief='groove')
-            frame.grid(row=dr, column=dc)
-            frame.place(x=20+(dc*275), y=20+(dr*70))
-            frame.pack_propagate(False)
-            label = tk.Label(frame, text=detail_lbls[detail_num], font=('Segoe Print', 12))
-            label.place(x=5, y=-1)
-            if detail_num == 2:
-                frm_dd = tk.Frame(frame, width=200, height=30)
-                frm_dd.place(x=5, y=30)
-                dd_placeholders = ['dd', 'mm', 'yyyy']
+    def load_order(file_name):
+        with open(f'{file_name}.json', 'r') as file:
+            order = json.load(file)
+            
+            nonlocal detail_save_lbls
+            for dl in range(8):
+                detail_ents[dl].insert(0, order[detail_save_lbls[dl]])
 
-                def delete_placeholder(event, d, error):
-                    if error == False:
-                        if detail_ents[d].get() == dd_placeholders[d-2]:
-                            detail_ents[d].delete(0, tk.END)
-                            detail_ents[d].config(fg='black')
-                    elif error == True:
-                        if detail_ents[d].get() == dd_placeholders[d-2]:
-                            detail_ents[d].delete(0, tk.END)
-                        detail_ents[d].config(fg='black')
+            create_tiers_table(int(order['tier_num']))
+            nonlocal tier_save_lbls
+            tload = 0
+            for tnum in range(int(order['tier_num'])):
+                for tl in range(3):
+                    tier_ents[tload].insert(0, order[f'tier{tnum+1}_{tier_save_lbls[tl]}'])
+                    tload += 1
 
-                def create_placeholder(event, d):
-                    if detail_ents[d].get() == '':
-                        detail_ents[d].insert(0, dd_placeholders[d-2])
-                        detail_ents[d].config(fg='gray')
-
-                for dd in range(3):
-                    detail_ents.append(tk.Entry(frm_dd, width=6, font=('Arial', 12), bg='white'))
-                    detail_ents[detail_num].pack(side='left')
-                    detail_ents[detail_num].insert(0, dd_placeholders[dd])
-                    detail_ents[detail_num].config(fg='gray')
-
-                    detail_ents[detail_num].bind('<FocusIn>', lambda event, d=detail_num: delete_placeholder(event, d, False))
-                    detail_ents[detail_num].bind('<FocusOut>', lambda event, d=detail_num: create_placeholder(event, d))
-                    detail_num += 1
-
-                frm_icon = tk.Frame(frame, width=21, height=21, bg='#FEF8A0')
-                frm_icon.place(x=167, y=30)
-                frm_icon.pack_propagate(False)
-                icon = tk.PhotoImage(file='date-icon.png')
-                img_icon = tk.Label(frm_icon, image=icon, bg='#FEF8A0')
-                img_icon.image = icon
-                img_icon.pack(fill="both", expand=True)
-            else:
-                detail_ents.append(tk.Entry(frame, width=20, font=('Arial', 12), bg='#FEF8A0'))
-                detail_ents[detail_num].place(x=5, y=30)
-                detail_num += 1
-    
-    # Task 2.3.1
-    frm_tiers = None
-
-    def create_tiers_table(rnum):
-        nonlocal frm_tiers
-        global tier_ents
-        global ent_tiers
-        if frm_tiers is not None:
-            frm_tiers.destroy()
-
-        frm_tiers = tk.Frame(content, width=265, height=106+(int(rnum)*25), bd=1.5, relief='groove')
-        frm_tiers.place(x=20, y=230)
-        frm_tiers.pack_propagate(False)
-        lbl_tiers = tk.Label(frm_tiers, text='No. of Cake Tiers:', font=('Segoe Print', 12))
-        lbl_tiers.place(x=5, y=-1)
-        ent_tiers = tk.Entry(frm_tiers, width=2, font=('Arial', 12))
-        ent_tiers.place(x=5, y=30)
-        ent_tiers.insert(0, rnum)
-
-        # Task 2.3.2 Tiers Table
-        # List of labels for the table
-        tier_lbls = [
-            'Tier:',
-            'Layers:',
-            'Size (In.):',
-            ''
-        ]
-
-        tier_ents = []
-
-        tier_num = 0
-
-        for tr in range(int(rnum)):
-            for tc in range(3):
-                if tr == 0:
-                    frm_lbl = tk.Frame(frm_tiers, width=84, height=38, bd=1.5, relief='groove', bg='#FEF8A0')
-                    frm_lbl.grid(row=0, column=tc)
-                    frm_lbl.place(x=4+(tc*84), y=60)
-                    frm_lbl.pack_propagate(False)
-                    lbl = tk.Label(frm_lbl, text=tier_lbls[tc], font=('Segoe Print', 12), bg='#FEF8A0')
-                    lbl.place(x=0, y=-1)
-
-                frame = tk.Frame(frm_tiers, width=84, height=25)
-                frame.grid(row=tr+1, column=tc)
-                frame.place(x=4+(tc*84), y=98+((tr)*25))
-                frame.pack_propagate(False)
-                if tc == 0:
-                    lbl_tier = tk.Label(frame, text=f'Tier {tr+1}', font=('Segoe Print', 12), bd=1.5, relief='groove')
-                    lbl_tier.pack(fill='both', expand=True)
-                elif tc == 1: 
-                    tier_ents.append(tk.Entry(frame, font=('Arial', 12), bd=1.5, relief='groove'))
-                    tier_ents[tier_num].pack(fill='both', expand=True)
-                    tier_num += 1
-                elif tc == 2:
-                    for s in range(2):
-                        frm_size = tk.Frame(frame, width=42, height=25)
-                        frm_size.place(x=42*s, y=0)
-                        frm_size.pack_propagate(False)
-                        tier_ents.append(tk.Entry(frm_size, font=('Arial', 12), bd=1.5, relief='groove'))
-                        tier_ents[tier_num].pack(fill='both', expand=True)
-                        tier_num += 1
-
-        # Task 2.3.1
-        frm_btn_tiers = tk.Frame(frm_tiers, width=22, height=22)
-        frm_btn_tiers.place(x=27, y=30)
-        frm_btn_tiers.pack_propagate(False)
-        btn_tiers = tk.Button(frm_btn_tiers, text='▼', font=('Arial', 10, 'bold'), bg='#FEF8A0', activebackground='#FEF8A0', command=lambda: create_tiers_table(ent_tiers.get()))
-        btn_tiers.pack(fill='both', expand=True)
-
-    create_tiers_table(1)
+    load_order('Jae')
 
     # Task 2.3.1
     frm_decor = tk.Frame(content, width=265, height=201, bd=1.5, relief='groove')
@@ -159,26 +55,20 @@ def open_orderlist(content):
         order_details = {}
         
         # Saving Details
-        detail_lbls = ['customer_details',
-                       'cake_flavour',
-                       'due_day',
-                       'due_month',
-                       'due_year',
-                       'cake_colours',
-                       'cake_type',
-                       'cake_shape'
-                       ]
+        nonlocal detail_save_lbls
 
         for dsave in range(8):
-            order_details[detail_lbls[dsave]] = detail_ents[dsave].get()
+            order_details[detail_save_lbls[dsave]] = detail_ents[dsave].get()
 
         # Saving Tiers Table
-        tier_lbls = ['layer', 'size_a', 'size_b']
+        nonlocal tier_save_lbls
         tsave = 0
+
+        order_details['tier_num'] = ent_tiers.get()
 
         for tnum in range(int(ent_tiers.get())):
             for td in range(3):
-                order_details[f'tier{tnum+1}_{tier_lbls[td]}'] = tier_ents[tsave].get()
+                order_details[f'tier{tnum+1}_{tier_save_lbls[td]}'] = tier_ents[tsave].get()
                 tsave += 1
 
         # Saving Decorations / Toppings
@@ -188,6 +78,7 @@ def open_orderlist(content):
             json.dump(order_details, f, indent=4)
 
         print('Saving...')
+        root.destroy()
 
     # Task 2.4.3 Error Prevention
     def error_prev():
@@ -282,9 +173,9 @@ def open_orderlist(content):
                 tier_ents[num].bind('<FocusIn>', lambda event, num=num: make_black(event, 't', num))
 
     # Task 2.4.4 Confirmation Pop-up
-    global confirm
     def confirm(type):
         # type = save / change
+        global root
         root = tk.Tk()
         root.title('Confirmation')
 
@@ -388,12 +279,157 @@ def open_orderlist(content):
     cvs_scroll.bind('<Enter>', lambda event: cvs_scroll.bind_all('<MouseWheel>', mouse_scrollbar))
     cvs_scroll.bind('<Leave>', lambda event: cvs_scroll.unbind_all('<MouseWheel>'))
 
-    frm_orderlist = tk.Frame(cvs_scroll)
+    frm_orderlist = tk.Frame(cvs_scroll, width=270)
     frm_orderlist.bind('<Configure>', lambda event: cvs_scroll.configure
-                       (scrollregion=cvs_scroll.bbox("all")))
+                       (scrollregion=cvs_scroll.bbox('all')))
     
     cvs_scroll.create_window((0, 0), window=frm_orderlist, anchor='nw')
 
+    def check_files():
+        global files
+        path = os.getcwd()
+        files = []
+        for f in os.listdir(path):
+            if f.endswith('.json'):
+                files.append(f)
+        print(files)
+
     # Task 2.6.3
-    for i in range(50):
-        tk.Button(frm_orderlist, text=f'Order {i+1}').pack(fill='x')
+    for i in range(4):
+        frm_order = tk.Frame(frm_orderlist, width=100, height=30, bd=1.5, relief='groove')
+        frm_order.pack(padx=80, pady=5)
+
+def create_details(content):
+    # Task 2.2 Details & Entry Boxes
+    detail_ent_lbls = [
+        'Customer Name:',
+        'Cake Flavour:',
+        'Due Date:',
+        '',
+        '',
+        'Cake Colour/s:',
+        'Cake Type:',
+        'Cake Shape:'
+    ]
+
+    detail_ents = []
+
+    detail_num = 0
+
+    for dr in range(3):
+        for dc in range(2):
+            frame = tk.Frame(content, width=265, height=62, bd=1.5, relief='groove')
+            frame.grid(row=dr, column=dc)
+            frame.place(x=20+(dc*275), y=20+(dr*70))
+            frame.pack_propagate(False)
+            label = tk.Label(frame, text=detail_ent_lbls[detail_num], font=('Segoe Print', 12))
+            label.place(x=5, y=-1)
+            if detail_num == 2:
+                frm_dd = tk.Frame(frame, width=200, height=30)
+                frm_dd.place(x=5, y=30)
+                dd_placeholders = ['dd', 'mm', 'yyyy']
+
+                def delete_placeholder(event, d, error):
+                    if error == False:
+                        if detail_ents[d].get() == dd_placeholders[d-2]:
+                            detail_ents[d].delete(0, tk.END)
+                            detail_ents[d].config(fg='black')
+                    elif error == True:
+                        if detail_ents[d].get() == dd_placeholders[d-2]:
+                            detail_ents[d].delete(0, tk.END)
+                        detail_ents[d].config(fg='black')
+
+                def create_placeholder(event, d):
+                    if detail_ents[d].get() == '':
+                        detail_ents[d].insert(0, dd_placeholders[d-2])
+                        detail_ents[d].config(fg='gray')
+
+                for dd in range(3):
+                    detail_ents.append(tk.Entry(frm_dd, width=6, font=('Arial', 12), bg='white'))
+                    detail_ents[detail_num].pack(side='left')
+                    detail_ents[detail_num].insert(0, dd_placeholders[dd])
+                    detail_ents[detail_num].config(fg='gray')
+
+                    detail_ents[detail_num].bind('<FocusIn>', lambda event, d=detail_num: delete_placeholder(event, d, False))
+                    detail_ents[detail_num].bind('<FocusOut>', lambda event, d=detail_num: create_placeholder(event, d))
+                    detail_num += 1
+
+                frm_icon = tk.Frame(frame, width=21, height=21, bg='#FEF8A0')
+                frm_icon.place(x=167, y=30)
+                frm_icon.pack_propagate(False)
+                icon = tk.PhotoImage(file='date-icon.png')
+                img_icon = tk.Label(frm_icon, image=icon, bg='#FEF8A0')
+                img_icon.image = icon
+                img_icon.pack(fill="both", expand=True)
+            else:
+                detail_ents.append(tk.Entry(frame, width=20, font=('Arial', 12), bg='#FEF8A0'))
+                detail_ents[detail_num].place(x=5, y=30)
+                detail_num += 1
+
+# Task 2.3.1
+frm_tiers = None
+def create_tiers_table(rnum):
+    global frm_tiers
+    if frm_tiers is not None:
+        frm_tiers.destroy()
+
+    frm_tiers = tk.Frame(content, width=265, height=106+(int(rnum)*25), bd=1.5, relief='groove')
+    frm_tiers.place(x=20, y=230)
+    frm_tiers.pack_propagate(False)
+    lbl_tiers = tk.Label(frm_tiers, text='No. of Cake Tiers:', font=('Segoe Print', 12))
+    lbl_tiers.place(x=5, y=-1)
+    global ent_tiers
+    ent_tiers = tk.Entry(frm_tiers, width=2, font=('Arial', 12))
+    ent_tiers.place(x=5, y=30)
+    ent_tiers.insert(0, rnum)
+
+    # Task 2.3.2 Tiers Table
+    # List of labels for the table
+    tier_ent_lbls = [
+        'Tier:',
+        'Layers:',
+        'Size (In.):',
+        ''
+    ]
+
+    global tier_ents
+    tier_ents = []
+
+    tier_num = 0
+
+    for tr in range(int(rnum)):
+        for tc in range(3):
+            if tr == 0:
+                frm_lbl = tk.Frame(frm_tiers, width=84, height=38, bd=1.5, relief='groove', bg='#FEF8A0')
+                frm_lbl.grid(row=0, column=tc)
+                frm_lbl.place(x=4+(tc*84), y=60)
+                frm_lbl.pack_propagate(False)
+                lbl = tk.Label(frm_lbl, text=tier_ent_lbls[tc], font=('Segoe Print', 12), bg='#FEF8A0')
+                lbl.place(x=0, y=-1)
+
+            frame = tk.Frame(frm_tiers, width=84, height=25)
+            frame.grid(row=tr+1, column=tc)
+            frame.place(x=4+(tc*84), y=98+((tr)*25))
+            frame.pack_propagate(False)
+            if tc == 0:
+                lbl_tier = tk.Label(frame, text=f'Tier {tr+1}', font=('Segoe Print', 12), bd=1.5, relief='groove')
+                lbl_tier.pack(fill='both', expand=True)
+            elif tc == 1: 
+                tier_ents.append(tk.Entry(frame, font=('Arial', 12), bd=1.5, relief='groove'))
+                tier_ents[tier_num].pack(fill='both', expand=True)
+                tier_num += 1
+            elif tc == 2:
+                for s in range(2):
+                    frm_size = tk.Frame(frame, width=42, height=25)
+                    frm_size.place(x=42*s, y=0)
+                    frm_size.pack_propagate(False)
+                    tier_ents.append(tk.Entry(frm_size, font=('Arial', 12), bd=1.5, relief='groove'))
+                    tier_ents[tier_num].pack(fill='both', expand=True)
+                    tier_num += 1
+
+    # Task 2.3.1
+    frm_btn_tiers = tk.Frame(frm_tiers, width=22, height=22)
+    frm_btn_tiers.place(x=27, y=30)
+    frm_btn_tiers.pack_propagate(False)
+    btn_tiers = tk.Button(frm_btn_tiers, text='▼', font=('Arial', 10, 'bold'), bg='#FEF8A0', activebackground='#FEF8A0', command=lambda: create_tiers_table(ent_tiers.get()))
+    btn_tiers.pack(fill='both', expand=True)
