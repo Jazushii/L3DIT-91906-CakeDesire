58e4ab5fee3bab37deded3d33f78ffcd4a0cefff
Jazushii <jazushii7@gmail.com>
2026-07-20 13:09:31 +1200

    Task 1.6.1
    ive finished the first table foundation, ill do the table functionality altogether so ill just duplicate and make another table

--- inventory.py
+++ inventory.py
@@ -18,11 +18,32 @@ def create_header(content, file_name):
     lbl_header.pack(fill='both', expand=True)
 
 def create_ingredients_table(content):
-    frm_title = tk.Frame(content)
+    frm_title = tk.Frame(content, width=200, height=40, bd=1.5, relief='groove', bg='#FFC957')
     frm_title.place(x=10, y=70)
     frm_title.pack_propagate(False)
 
+    lbl_title = tk.Label(frm_title, text='Ingredients Needed:', font=('Segoe Print', 12, 'bold'), bg='#FFC957')
+    lbl_title.pack()
+
+    ingreds_lbls = [
+        'Eggs:',
+        'Milk:',
+        'Cooking Oil:',
+        'Flour:',
+        'Sugar:',
+        'Butter:',
+        'Salt:',
+        'Baking Soda:',
+        'Cocoa Powder:',
+    ]
+
     for r in range(9):
         for c in range(2):
-            frame = tk.Frame(content)
-            frame.place()
+            frame = tk.Frame(content, width=100, height=30, bd=1.5, relief='groove')
+            frame.place(x=10+100*c, y=110+30*r)
+            frame.pack_propagate(False)
+
+            if c == 0:
+                frame.config(bg='#FEF8A0')
+                label = tk.Label(frame, text=ingreds_lbls[r], font=('Arial', 10), bg='#FEF8A0')
+                label.place(x=0, y=2)
